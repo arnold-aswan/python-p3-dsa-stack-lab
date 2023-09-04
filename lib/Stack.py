b6d877c15b2a531450b257a6592fe2e20fb36777
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-09-04 10:45:27 +0300

    completed assignment

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,37 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
-
+        self.items = items
+        self.limit = limit
+        
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if not self.full():
+            self.items.append(item)
 
     def pop(self):
-        pass
+        if not self.isEmpty():
+            item = self.items[-1]
+            self.items.pop()
+            return item
 
     def peek(self):
-        pass
+        return self.items[-1]
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) == self.limit
 
     def search(self, target):
-        pass
+        items = self.items[::-1]
+        
+        for i in range(len(items)):
+            if items[i] == target:
+                return i 
+        return -1
+        
+            
